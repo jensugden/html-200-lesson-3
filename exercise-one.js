7e4cfa76c7ef52a357f3dca2841530db59dbf1fc
Jenny Sugden <jensugden@gmail.com>
2020-01-29 20:50:12 -0800

    created exercise one array

---
+++ exercise-one.js
@@ -0,0 +1,15 @@
+let fiveElements = ['rain', 'fog', 'hail', 'snow', 'wind'];
+
+console.log(fiveElements);
+
+fiveElements.push('sun');
+
+console.log(fiveElements);
+
+fiveElements.splice(2,1);
+
+console.log(fiveElements);
+
+const fiveElementslist = fiveElements.join(', ');
+
+console.log(fiveElementslist);
